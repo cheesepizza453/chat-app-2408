a419419864e45591bd39e572ba90bd50d9e0c34d
cheesepizza453 <earlykim1209@gmail.com>
2024-08-25 10:12:58 +0900

    로컬 채팅 구현

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const express = require("express")
+const { WebSocketServer } = require("ws")
+const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.static("public"))
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(8000, () => {
+  console.log(`Example app listening on port 8000`)
+})
+
+const wss = new WebSocketServer({ port: 8001 })
+
+wss.broadcast = (message) => {
+    wss.clients.forEach((client) => {
+      client.send(message);
+    });
+  };
+  
+  wss.on("connection", (ws, request) => {
+    ws.on("message", (data) => {
+      wss.broadcast(data.toString());
+    });
+  
+    ws.on("close", () => {
+      wss.broadcast(`유저 한명이 떠났습니다. 현재 유저 ${wss.clients.size} 명`);
+    });
+  
+
+      wss.broadcast(
+        `새로운 유저가 접속했습니다. 현재 유저 ${wss.clients.size} 명`
+      );
 });
